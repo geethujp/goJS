5091a267697b44d55cfc7dd6dcd0ff521c1fc7c2
geethuj <geethuj@qburst.com>
2017-02-21 16:06:29 +0530

    Added color scheme

--- js/qb-pm.js
+++ js/qb-pm.js
@@ -1,226 +1,101 @@
- function init() {
-     var $ = go.GraphObject.make; // for conciseness in defining templates
-     myDiagram =
-         $(go.Diagram, "myDiagramDiv", // create a Diagram for the DIV HTML element
-             {
-                 // position the graph in the middle of the diagram
-                 // initialContentAlignment: go.Spot.Center,
-                 // allow Ctrl-G to call groupSelection()
-                 "commandHandler.archetypeGroupData": {
-                     text: "Group",
-                     isGroup: true,
-                     color: "blue"
-                 },
-                 // enable undo & redo
-                 "undoManager.isEnabled": true
-             });
-     // Dragging from the Shape will start drawing a new link.
-     myDiagram.nodeTemplate =
-         $(go.Node, "Horizontal",
-             $(go.Panel, "Auto",
-                 $(go.Shape, "RoundedRectangle", {
-                     fill: "#cce6ff", // the default fill, if there is no data-binding
-                     stroke: "#6699ff",
-                     height: 40,
-                     strokeWidth: 2,
-                     portId: "",
-                     cursor: "pointer", // the Shape is the port, not the whole Node
-                 }),
-                 $(go.TextBlock, {
-                         editable: true
-                     },
-                     new go.Binding("text", "key"))
-             ),
-             $("TreeExpanderButton")
-         );
-     myDiagram.layout = $(go.TreeLayout, { angle: 90 });
-     var nodeDataArray = [{ key: "QB_PM" }, {
-             key: "Manas Nair",
-             parent: "QB_PM",
-             color: "#000"
-         }, {
-             key: "Devi Chandrika",
-             parent: "QB_PM",
-         }, {
-             key: "Adani Shantigram Community Mobile App",
-             parent: "Manas Nair",
-         }, {
-             key: "Sunny Diamonds e-Commerce Platform",
-             parent: "Manas Nair"
-         }, //Adani Shantigram Community Mobile App
-         {
-             key: "Resources",
-             parent: "Adani Shantigram Community Mobile App"
-         }, {
-             key: "Project Managers",
-             parent: "Resources"
-         }, {
-             key: "Manooja Manoharan",
-             parent: "Project Managers"
-         }, {
-             key: "Saju Devasia",
-             parent: "Project Managers"
-         }, {
-             key: "Business Analyst",
-             parent: "Resources"
-         }, {
-             key: "Rohith Ramesh",
-             parent: "Business Analyst"
-         }, {
-             key: "Team Lead",
-             parent: "Resources"
-         }, {
-             key: "Al-Muneef Saifudeen",
-             parent: "Team Lead"
-         }, {
-             key: "Sajeer Noohukannu",
-             parent: "Team Lead"
-         }, {
-             key: "Manu Mohan",
-             parent: "Team Lead"
-         }, {
-             key: "Developer",
-             parent: "Resources"
-         }, {
-             key: "Santo Philip",
-             parent: "Developer"
-         }, {
-             key: "Ajeem Ashraf",
-             parent: "Developer"
-         }, {
-             key: "Deena Philip",
-             parent: "Developer"
-         }, {
-             key: "Akshaya Thazhathu",
-             parent: "Developer"
-         }, {
-             key: "Jayashankar Babu",
-             parent: "Developer"
-         }, {
-             key: "UX Designer",
-             parent: "Resources"
-         }, {
-             key: "Nandu Sreekumar",
-             parent: "UX Designer"
-         }, {
-             key: "Tester",
-             parent: "Resources"
-         }, {
-             key: "Shaira Nooh",
-             parent: "Tester"
-         }, {
-             key: "Designer",
-             parent: "Resources "
-         }, {
-             key: "Anoop Krishna",
-             parent: "Designer"
-         }, {
-             key: "Details",
-             parent: "Adani Shantigram Community Mobile App"
-         }, {
-             key: "Customer",
-             parent: "Details"
-         }, {
-             key: "Vertical",
-             parent: "Details"
-         }, {
-             key: "Project Category",
-             parent: "Details"
-         }, {
-             key: "Project Status",
-             parent: "Details"
-         }, {
-             key: "Technology",
-             parent: "Details"
-         }, {
-             key: "Description",
-             parent: "Details"
-         }, // Sunny Diamonds e-Commerce Platform
-         {
-             key: "Resources ",
-             parent: "Sunny Diamonds e-Commerce Platform"
-         }, {
-             key: "Project Managers ",
-             parent: "Resources "
-         }, {
-             key: "Vibhin Sreevalsan",
-             parent: "Project Managers "
-         }, {
-             key: "Saju Devasia ",
-             parent: "Project Managers "
-         }, {
-             key: "Business Analyst ",
-             parent: "Resources "
-         }, {
-             key: "Architect ",
-             parent: "Resources "
-         }, {
-             key: "Lekshmi Krishnan",
-             parent: "Architect "
-         },
-         {
-             key: "Rohith Gabriel",
-             parent: "Business Analyst "
-         }, {
-             key: "Team Lead ",
-             parent: "Resources "
-         }, {
-             key: "Abhilash Vijayan",
-             parent: "Team Lead "
-         }, {
-             key: "Module Lead ",
-             parent: "Resources "
-         }, {
-             key: "Nithal Ajayakumar",
-             parent: "Module Lead "
-         }, {
-             key: "Developer ",
-             parent: "Resources "
-         }, {
-             key: "Nimmi Sasidharan",
-             parent: "Developer "
-         }, {
-             key: "Sreya Paul",
-             parent: "Developer "
-         }, {
-             key: "Seena James",
-             parent: "Developer "
-         },  {
-             key: "UX Designer ",
-             parent: "Resources "
-         }, {
-             key: "Afthab Ali",
-             parent: "UX Designer "
-         }, {
-             key: "Tester ",
-             parent: "Resources "
-         }, {
-             key: "Anuja Murukesh",
-             parent: "Tester "
-         }, {
-             key: "Details ",
-             parent: "Sunny Diamonds e-Commerce Platform"
-         }, {
-             key: "Customer ",
-             parent: "Details "
-         }, {
-             key: "Vertical ",
-             parent: "Details "
-         }, {
-             key: "Project Category ",
-             parent: "Details "
-         }, {
-             key: "Project Status ",
-             parent: "Details "
-         }, {
-             key: "Technology ",
-             parent: "Details "
-         }, {
-             key: "Description ",
-             parent: "Details "
-         }
+function init() {
+    var $ = go.GraphObject.make; // for conciseness in defining templates
+    myDiagram =
+        $(go.Diagram, "myDiagramDiv", // create a Diagram for the DIV HTML element
+            {
+                // position the graph in the middle of the diagram
+                // initialContentAlignment: go.Spot.Center,
+                // allow Ctrl-G to call groupSelection()
+                "commandHandler.archetypeGroupData": {
+                    text: "Group",
+                    isGroup: true,
+                    color: "blue"
+                },
+                // enable undo & redo
+                "undoManager.isEnabled": true
+            });
+    // Dragging from the Shape will start drawing a new link.
+    myDiagram.nodeTemplate =
+        $(go.Node, "Horizontal",
+            $(go.Panel, "Auto",
+                $(go.Shape, "RoundedRectangle", {
+                    fill: "#cce6ff", // the default fill, if there is no data-binding
+                    stroke: "#6699ff",
+                    height: 40,
+                    strokeWidth: 2,
+                    portId: "",
+                    cursor: "pointer", // the Shape is the port, not the whole Node
+                },new go.Binding("fill", "fill")),
+                $(go.TextBlock, {
+                        editable: true
+                    },
+                    new go.Binding("text", "text"))
+            ),
+            $("TreeExpanderButton", { alignment: go.Spot.Bottom, alignmentFocus: go.Spot.Top }, { visible: true })
+        );
+    myDiagram.layout = $(go.TreeLayout, { angle: 90 });
+  var nodeDataArray = [
+            { key: 1, text: "QB_PM", fill: "#ff5722", stroke: "#4d90fe" },
+            { key: 2, text: "Manas Nair", fill: "#fdd835", stroke: "#8e24aa", parent: 1 },
+            { key: 3, text: "Devi Chandrika", fill: "#fdd835", stroke: "#8e24aa", parent: 1 },
+            { key: 4, text: "Adani Shantigram Community Mobile App", fill: "#c5e1a5", stroke: "#4d90fe", parent: 2 },
+            { key: 5, text: "Sunny Diamonds e-Commerce Platform", fill: "#c5e1a5", stroke: "#4d90fe", parent: 2 },
+            { key: 6, text: "Resources", fill: "#4db6ac", stroke: "#aed581", parent: 4 },
+            { key: 7, text: "Project Managers", fill: "#90a4ae", stroke: "#9ccc65", parent: 6 },
+            { key: 8, text: "Saju Devasia", fill: "#cfd8dc", stroke: "#4d90fe", parent: 7 },
+            { key: 9, text: "Business Analyst", fill: "#a1887f", stroke: "#9ccc65", parent: 6 },
+            { key: 10, text: "Rohith Ramesh", fill: "#d7ccc8", stroke: "#4d90fe", parent: 9 },
+            { key: 11, text: "Team Lead", fill: "#ff8a65", stroke: "#9ccc65", parent: 6 },
+            { key: 12, text: "Al-Muneef Saifudeen", fill: "#ffccbc", stroke: "#4d90fe", parent: 11 },
+            { key: 13, text: "Sajeer Noohukannu", fill: "#ffccbc", stroke: "#4d90fe", parent: 11 },
+            { key: 14, text: "Manu Mohan", fill: "#ffccbc", stroke: "#4d90fe", parent: 11 },
+            { key: 15, text: "Developer", fill: "#ffa726", stroke: "#9ccc65", parent: 6 },
+            { key: 16, text: "Santo Philip", fill: "#ffe0b2", stroke: "#4d90fe", parent: 15 },
+            { key: 17, text: "Ajeem Ashraf", fill: "#ffe0b2", stroke: "#4d90fe", parent: 15 },
+            { key: 18, text: "Deena Philip", fill: "#ffe0b2", stroke: "#4d90fe", parent: 15 },
+            { key: 19, text: "Jayashankar Babu", fill: "#ffe0b2", stroke: "#4d90fe", parent: 15 },
+            { key: 20, text: "UX Designer", fill: "#cddc39", stroke: "#9ccc65", parent: 6 },
+            { key: 21, text: "Nandu Sreekumar", fill: "#e6ee9c", stroke: "#4d90fe", parent: 20 },
+            { key: 22, text: "Tester", fill: "#00bcd4", stroke: "#9ccc65", parent: 6 },
+            { key: 23, text: "Shaira Nooh", fill: "#b2ebf2", stroke: "#4d90fe", parent: 22 },
+            { key: 24, text: "Designer", fill: "#e57373", stroke: "#9ccc65", parent: 6 },
+            { key: 25, text: "Anoop Krishna", fill: "#ffcdd2", stroke: "#4d90fe", parent: 24 },
+            { key: 26, text: "Details", fill: "#2196f3", stroke: "#aed581", parent: 4 },
+            { key: 27, text: "Customer", fill: "#f48fb1", stroke: "#9ccc65", parent: 26 },
+            { key: 28, text: "Vertical", fill: "#f48fb1", stroke: "#9ccc65", parent: 26 },
+            { key: 29, text: "Project Category", fill: "#f48fb1", stroke: "#9ccc65", parent: 26 },
+            { key: 30, text: "Project Status", fill: "#f48fb1", stroke: "#9ccc65", parent: 26 },
+            { key: 31, text: "Technology", fill: "#f48fb1", stroke: "#9ccc65", parent: 26 },
+            { key: 32, text: "Description", fill: "#f48fb1", stroke: "#9ccc65", parent: 26 },
+            { key: 33, text: "Resources", fill: "#4db6ac", stroke: "#aed581", parent: 5 },
+            { key: 34, text: "Project Managers", fill: "#90a4ae", stroke: "#9ccc65", parent: 33 },
+            { key: 35, text: "Saju Devasia", fill: "#cfd8dc", stroke: "#4d90fe", parent: 34 },
+            { key: 36, text: "Business Analyst", fill: "#a1887f", stroke: "#9ccc65", parent: 33 },
+            { key: 37, text: "Rohith Ramesh", fill: "#d7ccc8", stroke: "#4d90fe", parent: 36 },
+            { key: 38, text: "Team Lead", fill: "#ff8a65", stroke: "#9ccc65", parent: 33 },
+            { key: 39, text: "Al-Muneef Saifudeen", fill: "#ffccbc", stroke: "#4d90fe", parent: 38 },
+            { key: 40, text: "Sajeer Noohukannu", fill: "#ffccbc", stroke: "#4d90fe", parent: 38 },
+            { key: 41, text: "Manu Mohan", fill: "#ffccbc", stroke: "#4d90fe", parent: 38 },
+            { key: 42, text: "Developer", fill: "#ffa726", stroke: "#9ccc65", parent: 33 },
+            { key: 43, text: "Santo Philip", fill: "#ffe0b2", stroke: "#4d90fe", parent: 42 },
+            { key: 44, text: "Ajeem Ashraf", fill: "#ffe0b2", stroke: "#4d90fe", parent: 42 },
+            { key: 45, text: "Deena Philip", fill: "#ffe0b2", stroke: "#4d90fe", parent: 42 },
+            { key: 46, text: "Jayashankar Babu", fill: "#ffe0b2", stroke: "#4d90fe", parent: 42 },
+            { key: 47, text: "UX Designer", fill: "#cddc39", stroke: "#9ccc65", parent: 33 },
+            { key: 48, text: "Nandu Sreekumar", fill: "#e6ee9c", stroke: "#4d90fe", parent: 47 },
+            { key: 49, text: "Tester", fill: "#00bcd4", stroke: "#9ccc65", parent: 33 },
+            { key: 50, text: "Shaira Nooh", fill: "#b2ebf2", stroke: "#4d90fe", parent: 49 },
+            { key: 51, text: "Designer", fill: "#e57373", stroke: "#9ccc65", parent: 33 },
+            { key: 52, text: "Anoop Krishna", fill: "#ffcdd2", stroke: "#4d90fe", parent: 51 },
+            { key: 53, text: "Details", fill: "#2196f3", stroke: "#aed581", parent: 5 },
+            { key: 54, text: "Customer", fill: "#f48fb1", stroke: "#9ccc65", parent: 53 },
+            { key: 55, text: "Vertical", fill: "#f48fb1", stroke: "#9ccc65", parent: 53 },
+            { key: 56, text: "Project Category", fill: "#f48fb1", stroke: "#9ccc65", parent: 53 },
+            { key: 57, text: "Project Status", fill: "#f48fb1", stroke: "#9ccc65", parent: 53 },
+            { key: 58, text: "Technology", fill: "#f48fb1", stroke: "#9ccc65", parent: 53 },
+            { key: 59, text: "Description", fill: "#f48fb1", stroke: "#9ccc65", parent: 53 }
+        ]
 
+    myDiagram.model = new go.TreeModel(nodeDataArray);
 
-     ];
-     myDiagram.model = new go.TreeModel(nodeDataArray);
- }
+}
